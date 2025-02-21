76b33e7f3d3c965ed708260761ec4e24fa221830
Tushar Kukawada <tkukawada@gmail.com>
2025-02-20 19:59:25 -0500

    counter works

--- counter-app.js
+++ counter-app.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import { ref } from "lit/directives/ref.js";
 
 /**
  * `counter-app`
@@ -20,7 +21,8 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
+    this.count=0;
+    
     this.t = this.t || {};
     this.t = {
       ...this.t,
@@ -39,7 +41,7 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   static get properties() {
     return {
       ...super.properties,
-      title: { type: String },
+      count: { type: Number, reflect: true },
     };
   }
 
@@ -53,12 +55,18 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
+      :host([count="18"]) {
+        color: var(--ddd-theme-default-futureLime);
+      }
+      :host([count="21"]) {
+        color: var(--ddd-theme-default-futureLime);
+      }
       .wrapper {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
       }
-      h3 span {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+      .counter {
+        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xxl));
       }
     `];
   }
@@ -67,9 +75,22 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   render() {
     return html`
 <div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
+  <div class="counter">${this.count}</div>
+  <div class="buttons">
+    <button @click="${this.decrease}">-1</button>
+    <button @click="${this.increase}">+1</button>
+  </div>
 </div>`;
+  }
+
+  increase() {
+    this.count++;
+   }
+  decrease() {
+    this.count--;
+  }
+  reset () {
+    this.count = 0;
   }
 
   /**
